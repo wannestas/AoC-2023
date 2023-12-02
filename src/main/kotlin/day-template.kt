00b29983f9a558d303a3ab7f52a7e1f1be1a0bd7
Wannes Tas <taswannes@gmail.com>
2023-12-02 23:27:08 +0100

    Add day template

---
+++ src/main/kotlin/day-template.kt
@@ -0,0 +1,20 @@
+import kotlin.time.measureTimedValue
+
+fun main() {
+    val input = parseInput(getDayInput(2))
+    val (p1Solution, p1Time) = measureTimedValue { part1(input) }
+    println("Part 1 solution: $p1Solution , Time taken: $p1Time")
+    val (p2Solution, p2Time) = measureTimedValue { part2(input) }
+    println("Part 2 solution: $p2Solution , Time taken: $p2Time")
+}
+
+fun parseInput(input: List<String>): List<String> {
+    return input
+}
+
+fun part1(input: List<String>) {
+
+}
+fun part2(input: List<String>) {
+
+}
